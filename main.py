7201d7e91023c2400dd43143dffbf6d69c672a23
arush sharma <arushsharma91@gmail.com>
2020-06-29 06:27:53 +0530

    added current data

--- main.py
+++ main.py
@@ -6,17 +6,30 @@ import matplotlib.ticker as ticker
 from matplotlib.pyplot import figure
 from active_cases import retrieve
 
+st.title('Covid-19 in India')
 df = st.cache(pd.read_csv)('./Data/covid_19_india.csv')
+active_data = retrieve()
+st.dataframe(active_data)
 
 state = st.multiselect("Choose State", list(set(df['State/UnionTerritory'])), ["Kerala", "Delhi"])
 
-# if not state:
-#     st.error("Select at least one State")
+if not state:
+    st.error("Select at least one State")
+
 is_check = st.checkbox("Display Data")
+
 if is_check:
     for s in state:
         st.write(f'''## {s} ''')
+        st.write('''### Current Data''')
+     
         selected = df.loc[df['State/UnionTerritory'] == s,  ['Date', 'Confirmed', 'Deaths', 'Cured']]
+        selected_active = active_data.loc[active_data['state'] == s]
+
+        st.text(f'''Active : {selected_active.active.item()}''')
+        st.text(f'''Confirmed : {selected_active.confirmed.item()}''')
+        st.text(f'''Deaths : {selected_active.deaths.item()}''')
+        
         st.dataframe(selected)
         
         x = selected['Date']
